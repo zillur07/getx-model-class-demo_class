40d9bf22cfcfc75b6e53be3f2ca835822ed38f6c
zillur07 <zillur.info1@gmail.com>
2023-03-30 14:11:42 +0600

    change home page title

--- lib/src/view/pages/home_page.dart
+++ lib/src/view/pages/home_page.dart
@@ -8,7 +8,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
         child: Column(
